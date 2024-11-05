ee5b38dd436dc630365c5b732e7bfbcbeba6da11
gust <2414566866@qq.com>
2024-11-05 23:03:28 +0800

    sidebar ......

--- src/views/layout/GenLogin.vue
+++ src/views/layout/GenLogin.vue
@@ -1,16 +1,50 @@
 <template>
-  <div>
-    <h1>GenLogin</h1>
+  <div class="login-container">
+    <vs-dialog v-model="active">
+      <template #header><h4 class="not-margin"><b>gen-copilot</b></h4></template>
+      <div class="con-form">
+        <vs-input v-model="email" placeholder="Email">
+          <template #icon>@</template>
+        </vs-input>
+        <vs-input type="password" v-model="password" placeholder="Password">
+          <template #icon>
+            <i class='bx bxs-lock'></i>
+          </template>
+        </vs-input>
+        <div class="flex">
+          <vs-checkbox v-model="remember">Remember me</vs-checkbox>
+          <a href="#">Forgot Password?</a>
+        </div>
+      </div>
+      <template #footer>
+        <div class="footer-dialog">
+          <vs-button block>
+            Sign In
+          </vs-button>
+          <div class="new">
+            New Here? <a href="#">Create New Account</a>
+          </div>
+        </div>
+      </template>
+    </vs-dialog>
+    <img :src="LogoObj" alt="">
   </div>
 </template>
 
 <script>
+import Logo from '@/assets/logo.png';
 
 export default {
   name: "GenLogin",
   components: {},
   data() {
-    return {}
+    return {
+      LogoObj: Logo,
+      active: true,
+      email: '',
+      password: '',
+      remember: false
+    }
   },// data
   methods: {},// methods
   watch: {
@@ -23,4 +57,27 @@ export default {
 </script>
 
 <style scoped>
+[v-cloak] {
+  display: none;
+}
+
+.login-container {
+  overflow: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0px;
+  font-family: 'Rubik Mono One', sans-serif;
+  background: linear-gradient(to left, #00857d, #f50771);
+  /* Flexbox for centering */
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+}
+
+
+
 </style>
+
