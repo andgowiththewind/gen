90fc6470345ba29de23feeb1316ae9ea8e1131ba
gust <2414566866@qq.com>
2024-11-06 00:06:24 +0800

    ui ......

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,17 @@ module.exports = defineConfig({
             client: {overlay: false},
         },
     },
+    devServer: {
+        open: true,
+        proxy: {
+            // detail: https://cli.vuejs.org/config/#devserver-proxy
+            [process.env.VUE_APP_BASE_API]: {
+                target: `http://localhost:8848`,
+                changeOrigin: true,
+                pathRewrite: {
+                    ['^' + process.env.VUE_APP_BASE_API]: ''
+                }
+            }
+        },
+    },
 })
