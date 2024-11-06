4dd22ad11b342c6119e174adaebe4fb13b05471a
gust <2414566866@qq.com>
2024-11-06 11:57:27 +0800

    基础设施...

--- src/main/java/com/gust/cafe/gen/controller/LoginController.java
+++ src/main/java/com/gust/cafe/gen/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.gust.cafe.gen.controller;
 
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.extra.spring.SpringUtil;
 import com.gust.cafe.gen.config.threadlocal.LocaleHolder;
 import com.gust.cafe.gen.dto.core.R;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -15,6 +17,8 @@ public class LoginController {
     @PostMapping("/test")
     public R test(@RequestBody Map<String, Object> map) {
         Locale locale = LocaleHolder.getLocale();
+        MessageSource messageSource = SpringUtil.getBean(MessageSource.class);
+        String message = messageSource.getMessage("i18n_000000", null, LocaleHolder.getLocale());
         int a = 1 / 0;
         return R.data(IdUtil.simpleUUID().toLowerCase());
     }
