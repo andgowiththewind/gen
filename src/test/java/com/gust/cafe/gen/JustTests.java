161deafee34e465e098092c755986dc521ae16b9
gust <2414566866@qq.com>
2024-11-06 09:49:14 +0800

    基础设施...

--- src/test/java/com/gust/cafe/gen/JustTests.java
+++ src/test/java/com/gust/cafe/gen/JustTests.java
@@ -1,6 +1,5 @@
 package com.gust.cafe.gen;
 
-import cn.hutool.core.lang.id.NanoId;
 import cn.hutool.core.util.StrUtil;
 import com.gust.cafe.gen.util.GenIdUtil;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,7 @@ public class JustTests {
     @Test
     public void test001() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(StrUtil.format("i18n_{}", GenIdUtil.shortNanoId()));
+            System.out.println(StrUtil.format("i18n_{}=", GenIdUtil.shortNanoId()));
         }
     }
 }
