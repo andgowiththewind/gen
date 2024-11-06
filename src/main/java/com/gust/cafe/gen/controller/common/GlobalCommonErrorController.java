d63fbf759db79bbceea27f200a72d4b2dde64627
gust <2414566866@qq.com>
2024-11-06 09:35:06 +0800

    基础设施...

---
+++ src/main/java/com/gust/cafe/gen/controller/common/GlobalCommonErrorController.java
@@ -0,0 +1,24 @@
+package com.gust.cafe.gen.controller.common;
+
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class GlobalCommonErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public void handleError(HttpServletRequest request) throws Throwable {
+        String key = "javax.servlet.error.exception";
+        if (request.getAttribute(key) != null) {
+            log.debug(StrUtil.format("异常在{}被捕获", this.getClass().getSimpleName()));
+            // 再抛出以便让自定义的"全局异常处理器"(@RestControllerAdvice)捕获到并处理
+            throw (Throwable) request.getAttribute(key);
+        }
+    }
+}
---
+++ src/main/java/com/gust/cafe/gen/exception/GenServiceException.java
@@ -0,0 +1,39 @@
+package com.gust.cafe.gen.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.function.Consumer;
+
+/**
+ * 自定义业务异常
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class GenServiceException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private Integer code;
+    private String message;
+
+    public GenServiceException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+
+    /**
+     * 调用此方法运行的代码块，如果有异常则抛出指定的自定义异常{@link GenServiceException}
+     */
+    public static void wrapper(Consumer<Void> consumer) {
+        try {
+            consumer.accept(null);
+        } catch (Exception e) {
+            throw new GenServiceException(e.getMessage());
+        }
+    }
+
+}
