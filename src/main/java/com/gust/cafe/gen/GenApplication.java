b61a98a29f9e34e12cad27856a0246c5ecfdadf0
gust <2414566866@qq.com>
2024-11-06 15:45:00 +0800

    tk mapper

--- src/main/java/com/gust/cafe/gen/GenApplication.java
+++ src/main/java/com/gust/cafe/gen/GenApplication.java
@@ -1,13 +1,12 @@
 package com.gust.cafe.gen;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@MapperScan(basePackages = {"com.gust.cafe.gen.mapper"})
 @SpringBootApplication
+@tk.mybatis.spring.annotation.MapperScan(basePackages = {"com.gust.cafe.gen.mapper"})
 // @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // 不加载数据库配置
 public class GenApplication {
 
