e14f54baee77ccb3876727604a013af628b6d289
gust <2414566866@qq.com>
2024-11-06 09:50:19 +0800

    基础设施...

--- src/test/java/com/gust/cafe/gen/JustTests.java
+++ src/test/java/com/gust/cafe/gen/JustTests.java
@@ -8,7 +8,7 @@ public class JustTests {
     @Test
     public void test001() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(StrUtil.format("i18n_{}=", GenIdUtil.shortNanoId()));
+            System.out.println(StrUtil.format("i18n_{}", GenIdUtil.shortNanoId()));
         }
     }
 }
